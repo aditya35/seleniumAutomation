7eb659182d6815d41f1cebaf01e0c533667c347e
Aditya <adityashekhar35@gmail.com>
2020-10-04 22:08:14 +0530

    Inital commit

---
+++ src/tip_selenium/DataExcel.java
@@ -0,0 +1,100 @@
+package tip_selenium;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DataExcel {
+	
+	
+	public void display(WebDriver driver) throws IOException {
+		
+		
+		//To store the names of flights
+		String str[]=new String[5];
+		
+		//To store the price
+		String pri[]=new String[5];
+		
+		//List for flight names
+		List<WebElement> name= (List<WebElement>) driver.findElements(By.xpath("//*[contains(@class,'col-md-7 col-sm-7 padd-lft airl-txt-n')]"));
+		
+		//List for flight price
+		List<WebElement> price=driver.findElements(By.xpath("//*[contains(@class,'col-md-8 col-sm-8 col-xs-9 txt-r6-n ng-binding')]"));
+		
+		int j=0;
+		for(int i=0;i<5;i++)
+		{
+			//Printing the details
+			if(!price.get(j).getText().equals(""))
+			{
+				System.out.println("Flight Name :"+name.get(i).getText()+"-----Price : "+price.get(j).getText());
+				j=j+2;
+			}
+			
+		}
+		
+		XSSFWorkbook workbook=new XSSFWorkbook();
+		  XSSFSheet sheet=workbook.createSheet("sheet");
+		 
+		 //To input the headings in excel
+		  XSSFRow firstRow=sheet.createRow(0);
+		  XSSFCell firstCell=firstRow.createCell(0);
+		  firstCell.setCellValue("Flight Name");
+		  XSSFCell SecondCell=firstRow.createCell(1);
+		  SecondCell.setCellValue("Price");
+		 
+		 //To input first flight's name and price
+		  XSSFRow SecondRow=sheet.createRow(1);
+		  XSSFCell SecondFirstCell=SecondRow.createCell(0);
+		  SecondFirstCell.setCellValue(name.get(0).getText() );
+		  XSSFCell SecondSecondCell=SecondRow.createCell(1);
+		  SecondSecondCell.setCellValue(price.get(0).getText() );
+		 
+		//To input second flight's name and price
+		  XSSFRow ThirdRow=sheet.createRow(2);
+		  XSSFCell ThirdFirstCell=ThirdRow.createCell(0);
+		  ThirdFirstCell.setCellValue(name.get(1).getText() );
+		  XSSFCell ThirdSecondCell=ThirdRow.createCell(1);
+		  ThirdSecondCell.setCellValue(price.get(2).getText() );
+		 
+		  //To input third flight's name and price
+		  XSSFRow FourthRow=sheet.createRow(3);
+		  XSSFCell FourthFirstCell=FourthRow.createCell(0);
+		  FourthFirstCell.setCellValue(name.get(2).getText() );
+		  XSSFCell FourthSecondCell=FourthRow.createCell(1);
+		  FourthSecondCell.setCellValue(price.get(4).getText() );
+		 
+		//To input fourth flight's name and price
+		  XSSFRow FifthRow=sheet.createRow(4);
+		  XSSFCell FifthFirstCell=FifthRow.createCell(0);
+		  FifthFirstCell.setCellValue(name.get(3).getText() );
+		  XSSFCell FifthSecondCell=FifthRow.createCell(1);
+		  FifthSecondCell.setCellValue(price.get(6).getText() );
+		 
+		//To input fifth flight's name and price
+		  XSSFRow SixthRow=sheet.createRow(5);
+		  XSSFCell SixthFirstCell=SixthRow.createCell(0);
+		  SixthFirstCell.setCellValue(name.get(4).getText() );
+		  XSSFCell SixthSecondCell=SixthRow.createCell(1);
+		  SixthSecondCell.setCellValue(price.get(8).getText() );
+		 
+		  FileOutputStream writeFile=new FileOutputStream("Mini_Project.xlsx");
+		  workbook.write(writeFile);
+		
+	}
+}
---
+++ src/tip_selenium/Trip.java
@@ -0,0 +1,142 @@
+package tip_selenium;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Trip {
+	
+	static WebDriver driver;
+	WebDriverWait wait;
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+    XSSFCell cell;
+    String  sr;
+    String dc;
+    
+ 
+	//Driver setup	
+	public WebDriver createDriver() {
+		DriverSetup d=new DriverSetup();
+		driver=d.getWebDriver();
+		return driver;
+	}
+	
+	//Access the website
+	public void websiteAccess() {
+		
+				
+		driver.navigate().to("https://www.easemytrip.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.navigate().refresh();
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+	}
+	
+	//Get input data from the excel 
+	public void getData() throws IOException{
+		
+		//Set the location of the excel file
+		File src=new File("C:\\Users\\870653\\Documents\\tripexcel.xlsx");
+	      
+	     // Load the file.
+	     FileInputStream finput = new FileInputStream(src);
+	      
+	     // Load he workbook.
+	    workbook = new XSSFWorkbook(finput);
+	      
+	     // Load the sheet in which data is stored.
+	     sheet= workbook.getSheetAt(0);
+	      
+	     
+	    // Import data for From(source).
+         cell = sheet.getRow(1).getCell(0);
+         sr=cell.getStringCellValue();
+         
+        // Import data for To(destination).
+         cell = sheet.getRow(1).getCell(1);
+         dc=cell.getStringCellValue();
+	}
+	
+	public void travelDetails() {
+		
+		//Match the from value
+		driver.findElement(By.id("FromSector_show")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'"+sr+"')]")).click();
+		
+		//Match the To value
+		driver.findElement(By.id("Editbox13_show")).click();
+		driver.findElement(By.xpath("//div[@id='toautoFill']//span[contains(text(),'"+dc+"')]")).click();
+		
+		//Selection of date- 25/09/2020
+		driver.findElement(By.id("ddate")).click();
+		driver.findElement(By.id("frth_5_25/09/2020")).click();
+		
+		driver.findElement(By.id("search")).click();
+		
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	
+	}
+	
+	//Sorting the flights in decreasing order of the price
+	public void sortingResult(){
+		
+		driver.navigate().refresh();
+		
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//*[contains(@class,'fa pri2 fa-long-arrow-up pad-4')]")).click();
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[contains(@class,'fa pri2 pad-4 fa-long-arrow-down')]")).click();
+	}
+	
+	//To display the name and price in console and excel
+	public void displayExcel() {
+		
+		
+		DataExcel de=new DataExcel();
+		try {
+			de.display(driver);
+		}
+		catch(Exception e) {
+			System.out.println("Exception handled");
+		}
+	}
+	
+	//Closing the window
+	public void closeWindow() {
+		
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.close();
+	}
+	
+	
+	public static void main(String[] args){
+		Trip obj=new Trip();
+		obj.createDriver();
+		
+		obj.websiteAccess();
+		try{
+			 obj.getData();
+		 }
+		 catch(Exception e){
+			 System.out.println("Exception handled");
+		 }
+		obj.travelDetails();
+		obj.sortingResult();
+		obj.displayExcel();
+		obj.closeWindow();
+	}
+}
